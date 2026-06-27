218b00ef31c708b2e0f858c13dddb5444362e45d
Rohit Goswami <r95g10@gmail.com>
2026-06-27 10:13:08 -0500

    chore: ignore target/

--- target/debug/deps/readcon_db-2af7cfa24c1809c0.d
+++
@@ -1,5 +0,0 @@
-/home/rgoswami/Git/Github/LODE/readcon-db/target/debug/deps/readcon_db-2af7cfa24c1809c0.d: src/lib.rs
-
-/home/rgoswami/Git/Github/LODE/readcon-db/target/debug/deps/readcon_db-2af7cfa24c1809c0: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/readcon_db-ba4bd284e1e5dda4.d
+++
@@ -1,7 +0,0 @@
-/home/rgoswami/Git/Github/LODE/readcon-db/target/debug/deps/readcon_db-ba4bd284e1e5dda4.d: src/lib.rs
-
-/home/rgoswami/Git/Github/LODE/readcon-db/target/debug/deps/libreadcon_db-ba4bd284e1e5dda4.rlib: src/lib.rs
-
-/home/rgoswami/Git/Github/LODE/readcon-db/target/debug/deps/libreadcon_db-ba4bd284e1e5dda4.rmeta: src/lib.rs
-
-src/lib.rs:
